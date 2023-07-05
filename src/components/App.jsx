5b4a92905cc8f3dec621ee857dbfbbfd36692df7
kadoryFT <72969287+kadoryFT@users.noreply.github.com>
2023-07-05 08:01:12 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [talk] = useState({
+    one: "الله اكبر ",
+    two: " الحمدلله",
+    three: " سبحان الله"
+  });
+  function increase() {
+    setCount(count + 1);
+    if (count === 100) {
+      setCount(0);
+    }
+  }
+
+  function restet() {
+    setCount(0);
+  }
+  return (
+    <div className="container">
+      <div className="meow">
+        <pre>
+          <h1>
+            {count <= 34
+              ? talk.one
+              : count <= 67
+              ? talk.two
+              : count > 67
+              ? talk.three
+              : null}
+          </h1>
+        </pre>
+      </div>
+      <h1>{count}</h1>
+      <button type="button" class="btn btn-outline-success" onClick={increase}>
+        +
+      </button>
+      <div></div>
+      <button type="button" class="btn btn-outline-success" onClick={restet}>
+        re
+      </button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav class="navbar navbar-light bg-dark ">
+      <span class="navbar-brand mb-0 h1 ">مسبحة</span>
+    </nav>
+  );
+}
+
+export default Nav;
