a4621d358c8c7bef19a48a85fc35dbc645701ec1
kadoryFT <72969287+kadoryFT@users.noreply.github.com>
2023-07-09 01:32:15 +0300

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
   return (
     <nav class="navbar navbar-light bg-dark ">
-      <span class="navbar-brand mb-0 h1 ">مسبحة</span>
+      <span class="navbar-brand mb-0 h1 "> الزهراء  مسبحة</span>
     </nav>
   );
 }
